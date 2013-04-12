e9ee423112405dda824196740612ed91122854a6
Johan Carlsson <johancarlsson@me.com>
2013-04-12 23:00:43 +0200

    Changed the table row behavior depending on if the user is logged in

--- app/helpers/provider_contacts_helper.rb
+++ app/helpers/provider_contacts_helper.rb
@@ -1,2 +1,9 @@
 module ProviderContactsHelper
+  def provider_contact_table_row_url(provider,provider_contact)
+    if current_user
+      provider_contact_url(provider,provider_contact)
+    else
+      contact_url(provider_contact.contact)
+    end
+  end
 end
